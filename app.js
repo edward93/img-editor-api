2d8009cf0f1293bbac853ad0654b9825acd52eca
edward93 <edward.chopuryan@gmail.com>
2022-08-05 16:23:08 -0400

    enabling cors *

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var cors = require("cors");
 
 var path = require("path");
 var cookieParser = require("cookie-parser");
@@ -9,12 +10,12 @@ var swaggerRouter = require("./routes/swaggerRouter");
 
 var app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
 
 app.use("/", swaggerRouter);
 app.use("/api", imgEditorRouter);
